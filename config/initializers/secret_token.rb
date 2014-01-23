cfea07271e0d2fbc795f35f8b82ede89c10e04d4
Cavin Stone <cstone2003@hotmail.com>
2014-01-22 22:13:39 -0600

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Achatesadvisors::Application.config.secret_token = '69e8c7ed3f43422ccc4b2a537b333de6785b141acfd576f9727890e7a2071ff997e95da277f39bb55f01a4c059aa5f05f5ed08b8e8784101a0ca0f4cd23dd82b'
