946cb1ad5ec9ac1760f6a470a715fe80d84854ed
Cavin Stone <cstone2003@hotmail.com>
2014-01-26 15:26:01 -0600

    Update AWS Region

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -4,6 +4,7 @@ CarrierWave.configure do |config|
       :provider               => 'AWS',                        # required
       :aws_access_key_id      => ENV["AWS_ACCESS_KEY_ID"],     # required
       :aws_secret_access_key  => ENV["AWS_SECRET_ACCESS_KEY"], # required
+      :region => ENV["S3_REGION"]
   }
   config.fog_directory  = ENV["AWS_S3_BUCKET"]                     # required
   config.fog_public     = true                                # optional, defaults to true
